2122dcb82fb462cd2dc0fdba86426b81937c6c33
Fabrice Mulumba <fabreezem@gmail.com>
2018-07-14 20:12:23 -0400

    Added routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 import Intro from '../Intro/Intro';
@@ -32,16 +34,28 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
+
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome {this.state.welcomeName}</h1>
         </header>
 
-          <Intro />
-          <Form nameFunctionProps={this.nameChange}/>
-          <Search />
+          <li>
+            <Link to="/form">Bio</Link>
+          </li>
+
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+
+          <Route exact path="/" component={Intro} />
+          <Route path="/form" render={(nameChange) => ( <Form nameFunctionProps={this.nameChange}/> )} />
+          <Route path="/search" component={Search} />
       </div>
+      </Router>
     );
   }
 }
